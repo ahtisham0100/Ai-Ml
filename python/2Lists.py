dadacb3b4908dc2ac3238b517c3c627e8243a3b7
Muhammad Ahtisham Arif <ahtisham0100@gmail.com>
2025-07-10 19:39:07 +0500

    intro to py

---
+++ python/2Lists.py
@@ -0,0 +1,32 @@
+# list = ["item" , "item2", "item3", "item4", "item5"] 
+
+# print(list)  # ['item', 'item2', 'item3', 'item4', 'item5'] 
+
+# for item in list:
+#     print(item)
+
+# print(len(list))
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+print(list1)  # ['apple', 'banana', 'cherry']
+print(list2)  # [1, 5, 7, 9,    3]
+print(list3)  # [True, False, False]
+
+mixed_list = ["apple", 1, True, 3.14]
+print(mixed_list)  # ['apple', 1, True, 3.14
+
+print(type(mixed_list)) 
+
+## list contructor : 
+
+
+
+print(list1[1:3])  # apple
+
+numbers= []
+for i in range (0,10):
+        numbers.append(i)
+print(numbers)  # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(numbers[5:len(numbers)])  # [0, 1, 2, 3, 4]
